adc1e34b8a047bb89796424cbcc46f55504f8d6d
Mohammed Sabir <127587424+sabz47@users.noreply.github.com>
2026-01-15 23:42:21 +0400

    Create Load.sql

---
+++ scripts/Load.sql
@@ -0,0 +1,72 @@
+TRUNCATE TABLE  bronze.crm_cust_info;
+
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\Users\mm354\Downloads\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+SELECT COUNT(*) 
+FROM  bronze.crm_cust_info;
+
+
+TRUNCATE TABLE  bronze.crm_sales_details;
+
+BULK INSERT bronze.crm_sales_details
+FROM 'C:\Users\mm354\Downloads\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+select count(*) from  bronze.crm_sales_details;
+
+TRUNCATE TABLE  bronze.crm_prd_info;
+
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\Users\mm354\Downloads\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+select count(*) from bronze.crm_prd_info;
+
+TRUNCATE TABLE  bronze.erp_cust_az12;
+
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\Users\mm354\Downloads\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+select count(*) from bronze.erp_cust_az12;
+
+TRUNCATE TABLE  bronze.erp_loc_a101;
+
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:\Users\mm354\Downloads\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+select count(*) from bronze.erp_loc_a101;
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\Users\mm354\Downloads\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+select count(*) from bronze.erp_px_cat_g1v2;
