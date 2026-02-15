94deadb120fe3b76c195013ea71dcb20a4bc0213
Mohammed Sabir <127587424+sabz47@users.noreply.github.com>
2026-02-15 23:05:37 +0400

    Update Load_Procedure.sql

--- scripts/bronze/Load_Procedure.sql
+++ scripts/bronze/Load_Procedure.sql
@@ -1,17 +1,4 @@
 /*
-===============================================================================
-DDL Script: Create Bronze Tables
-===============================================================================
-Script Purpose:
-    This script creates tables in the 'bronze' schema, dropping existing tables 
-    if they already exist.
-Run this script to re-define the DDL structure of 'bronze' Tables
-
-BULK INSERT: It is a method of loading massive amount of data very quickly from files like CSV files or text files directly into a database.
-===============================================================================
-*/
-
-/*
 FIRSTROW=2 WE ARE TELLING SQL THAT SKIP FIRST ROW,BCZ IN SOURCE FILES WHICH IS CSV FIRST ROW IS HEADER.
 
 FILE DELIMETER(Terminator): File Seperator  
@@ -24,7 +11,19 @@ QUALITY CHECK: CHECK THAT DATA IS NOT SHIFTED AND IS IN THE CORRECT COLUMN
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME;
+	BEGIN TRY
+print '=========================================================================';
+print 'Loading Bronze Layer';
+print '=========================================================================';
+
+print '-------------------------------------------------------------------------';
+print 'Loading CRM Tables';
+print '-------------------------------------------------------------------------';
+SET @start_time = GETDATE();
+print 'Truncating Table: bronze.crm_cust_info';
 	TRUNCATE TABLE bronze.crm_cust_info;
+print 'Inserting Data into: bronze.crm_cust_info';
 	BULK INSERT bronze.crm_cust_info
 	FROM 'C:\Users\mm354\Downloads\GITHUB PROJECTS\SQL DATAWAREHOUSE\datasets\source_crm\cust_info.csv'
 	WITH (
@@ -32,8 +31,14 @@ BEGIN
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
-
+SET @end_time = GETDATE();
+print '---------------------------------------------------------------------------';
+PRINT '>>Load Duration ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + ' seconds';
+print '<<------------------------------------->>';
+SET @start_time = GETDATE();
+print 'Truncating Table: bronze.crm_prd_info';
 	TRUNCATE TABLE bronze.crm_prd_info;
+print 'Inserting Data into: bronze.crm_prd_info';
 	BULK INSERT bronze.crm_prd_info
 	FROM 'C:\Users\mm354\Downloads\GITHUB PROJECTS\SQL DATAWAREHOUSE\datasets\source_crm\prd_info.csv'
 	WITH (
@@ -41,8 +46,14 @@ BEGIN
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
-
+SET @end_time = GETDATE();
+print '---------------------------------------------------------------------------';
+PRINT 'Load Duration ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + ' seconds';
+print '<<------------------------------------->>';
+SET @start_time = GETDATE();
+print 'Truncating Table: bronze.crm_sales_details';
 	TRUNCATE TABLE bronze.crm_sales_details;
+print 'Inserting Data into: bronze.crm_sales_details';
 	BULK INSERT bronze.crm_sales_details
 	FROM 'C:\Users\mm354\Downloads\GITHUB PROJECTS\SQL DATAWAREHOUSE\datasets\source_crm\sales_details.csv'
 	WITH (
@@ -50,8 +61,18 @@ BEGIN
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
+SET @end_time = GETDATE()
+print '---------------------------------------------------------------------------';
+PRINT 'Load Duration ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + ' seconds';
+print '<<------------------------------------->>';
+print 'Loading ERP Tables';
 
+print '-------------------------------------------------------------------------';
+SET @start_time = GETDATE();
+print '-------------------------------------------------------------------------';
+print '>> Truncating Table: bronze.erp_cust_az12';
 	TRUNCATE TABLE bronze.erp_cust_az12;
+print '>> Inserting Data into: bronze.erp_cust_az12';
 	BULK INSERT bronze.erp_cust_az12
 	FROM 'C:\Users\mm354\Downloads\GITHUB PROJECTS\SQL DATAWAREHOUSE\datasets\source_erp\CUST_AZ12.csv'
 	WITH (
@@ -59,8 +80,16 @@ BEGIN
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
+SET @end_time = GETDATE();
 
+print '---------------------------------------------------------------------------';
+PRINT 'Load Duration ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + ' seconds';
+print '<<------------------------------------->>';
+
+SET @start_time = GETDATE();
+print '>> Truncating Table: bronze.erp_loc_a101';
 	TRUNCATE TABLE bronze.erp_loc_a101;
+print '>> Inserting Data into: bronze.erp_loc_a101';
 	BULK INSERT bronze.erp_loc_a101
 	FROM 'C:\Users\mm354\Downloads\GITHUB PROJECTS\SQL DATAWAREHOUSE\datasets\source_erp\loc_a101.csv'
 	WITH (
@@ -68,8 +97,15 @@ BEGIN
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
+SET @end_time = GETDATE();
 
+PRINT '---------------------------------------------------------------------------------';
+PRINT 'Loading Duration ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + ' seconds';
+print '<<------------------------------------->>';
+SET @start_time = GETDATE();
+print '>> Truncating Table: bronze.erp_px_cat_g1v2';
 	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+print '>> Inserting Data into: bronze.erp_px_cat_g1v2';
 	BULK INSERT bronze.erp_px_cat_g1v2
 	FROM 'C:\Users\mm354\Downloads\GITHUB PROJECTS\SQL DATAWAREHOUSE\datasets\source_erp\px_cat_g1v2.csv'
 	WITH (
@@ -77,6 +113,19 @@ BEGIN
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
+print '---------------------------------------------------------------------------';
+print 'Loading Duration ' + CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + ' seconds';
+print '<<------------------------------------->>';
+	END TRY
+	BEGIN CATCH
+	PRINT '======================================================';
+	PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+	PRINT 'Error Message' + ERROR_MESSAGE();	
+	PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+	PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+	PRINT '======================================================';
+	END CATCH
 END
 
 EXEC bronze.load_bronze;
+
